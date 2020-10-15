f4f469360f389a39ab585d81797603ba080a6dc7
joseriera97 <josealbertoriera@paucasesnovescifp.cat>
2020-10-15 20:56:40 +0200

    Ejercicio 9. Cada boton ejecuta una funcion que recorre del 0 al 10 para obtener su tipo de numeros par/Impar. Cuando ya los tiene , los imprime en el Combobox. Y para finalizar si se selecciona un item en el combobox es impreso en el TextBox.

---
+++ Exercice9/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Exercice9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.numerosComboBox = new System.Windows.Forms.ComboBox();
+            this.parButton = new System.Windows.Forms.Button();
+            this.imparButton = new System.Windows.Forms.Button();
+            this.numeroTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(25, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 24);
+            this.label7.TabIndex = 55;
+            this.label7.Text = "Exercice 9";
+            // 
+            // numerosComboBox
+            // 
+            this.numerosComboBox.FormattingEnabled = true;
+            this.numerosComboBox.Location = new System.Drawing.Point(106, 77);
+            this.numerosComboBox.Name = "numerosComboBox";
+            this.numerosComboBox.Size = new System.Drawing.Size(121, 21);
+            this.numerosComboBox.TabIndex = 56;
+            this.numerosComboBox.SelectedIndexChanged += new System.EventHandler(this.numerosComboBox_SelectedIndexChanged);
+            // 
+            // parButton
+            // 
+            this.parButton.Location = new System.Drawing.Point(327, 77);
+            this.parButton.Name = "parButton";
+            this.parButton.Size = new System.Drawing.Size(127, 23);
+            this.parButton.TabIndex = 57;
+            this.parButton.Text = "Even(Par)";
+            this.parButton.UseVisualStyleBackColor = true;
+            this.parButton.Click += new System.EventHandler(this.parButton_Click);
+            // 
+            // imparButton
+            // 
+            this.imparButton.Location = new System.Drawing.Point(503, 77);
+            this.imparButton.Name = "imparButton";
+            this.imparButton.Size = new System.Drawing.Size(127, 23);
+            this.imparButton.TabIndex = 58;
+            this.imparButton.Text = "Odd(Impar)";
+            this.imparButton.UseVisualStyleBackColor = true;
+            this.imparButton.Click += new System.EventHandler(this.imparButton_Click);
+            // 
+            // numeroTextBox
+            // 
+            this.numeroTextBox.Location = new System.Drawing.Point(106, 236);
+            this.numeroTextBox.Name = "numeroTextBox";
+            this.numeroTextBox.Size = new System.Drawing.Size(524, 20);
+            this.numeroTextBox.TabIndex = 59;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 308);
+            this.Controls.Add(this.numeroTextBox);
+            this.Controls.Add(this.imparButton);
+            this.Controls.Add(this.parButton);
+            this.Controls.Add(this.numerosComboBox);
+            this.Controls.Add(this.label7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox numerosComboBox;
+        private System.Windows.Forms.Button parButton;
+        private System.Windows.Forms.Button imparButton;
+        private System.Windows.Forms.TextBox numeroTextBox;
+    }
+}
+
---
+++ Exercice9/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Conseguir los numeros pares
+        private void parButton_Click(object sender, EventArgs e)
+        {
+            //Vaciar los items que tengamos en el ListBox
+            numerosComboBox.Items.Clear();
+
+            //load a list, t, with 10 integers
+            List<int> t = Enumerable.Range(0, 10).ToList();
+
+            //find even numbers
+            var evenNumbers = t.Where(num => num % 2 == 0);
+
+            //Poner los numeros pares
+            foreach (int i in evenNumbers)
+                numerosComboBox.Items.Add(i);
+
+
+        }
+
+        //Conseguir los numeros impares
+        private void imparButton_Click(object sender, EventArgs e)
+        {
+            //Vaciar los items que tengamos en el ListBox
+            numerosComboBox.Items.Clear();
+
+            //load a list, t, with 10 integers
+            List<int> t = Enumerable.Range(0, 10).ToList();
+
+            //find odd numbers
+            var oddNumbers = t.Where(num => num % 2 != 0);
+
+            //print odd numbers
+            foreach (int i in oddNumbers)
+                numerosComboBox.Items.Add(i);
+
+
+        }
+
+        private void numerosComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            numeroTextBox.Text = numerosComboBox.SelectedItem.ToString();
+        }
+    }
+}
